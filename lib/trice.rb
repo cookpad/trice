d8c5d8717ef086d6e130611f6189338b2f377dbf
moro <kyosuke-morohashi@cookpad.com>
2016-02-03 17:30:53 +0900

    Fix non-load problem

--- lib/trice.rb
+++ lib/trice.rb
@@ -6,6 +6,10 @@ require 'trice/reference_time'
 require 'trice/reference_time_accessor'
 require 'trice/version'
 
+if defined?(Rails)
+  require 'trice/railtie'
+end
+
 module Trice
   autoload 'SpecHelper', 'trice/spec_helper'
 
